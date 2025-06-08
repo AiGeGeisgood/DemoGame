09a6680035ab6fbac490a71d63cd5e285171cfe2
Aigege <12345678@qq.com>
2025-06-08 15:42:43 +0800

    UE5.5版本转换

--- Source/DemoGame.Target.cs
+++ Source/DemoGame.Target.cs
@@ -9,7 +9,6 @@ public class DemoGameTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("DemoGame");
 	}
 }
